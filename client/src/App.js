4b93965a99c50cb676ba1b187b270422cda7f81e
Serhii Dimchenko <serhii.dimchenko@pm.bet>
2022-01-18 21:22:13 +0200

    "Initial commit"

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            HELLO FROM ME!
+            Learn React 3
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
